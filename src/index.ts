16eecd58210c219cd566309657d1fafefe617dc6
Diwo777 <diego.zegarra79@icloud.com>
2021-04-19 18:37:53 -0400

    added mappable-interface

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
-import { User } from './User'
-import { Company } from './Company'
+// import { User } from './User'
+// import { Company } from './Company'
+import { CustomMap } from './CustomMaps'
 
-const user = new User();
-console.log(user)
 
-const company = new Company();
-console.log(company)
+new CustomMap('map');
+
